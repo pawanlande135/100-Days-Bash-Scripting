204c39710f8aec488a78face63f2e8f02dcecf5d
user <user@ymul.com>
2025-10-07 14:24:49 +0000

    Updated Master

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
-#Write a script which will grep "error" in all the "*.log" files at /var/log directory 
-#you need to print the unique file names which are not having "error" string in them 
-#and remove duplicate file names from output ?
-
-
 #!/usr/bin/bash
 
-grep -rL "error" /var/log/*.log | sort -u       
+function area() {
 
+area=$(($1*$2))
+echo "Area of rectangle is: $area"
 
+}
+
+area 20 121
