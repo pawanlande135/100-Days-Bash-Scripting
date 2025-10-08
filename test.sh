5a78156aedf1fd3b880039f13e49f1f713846a7c
user <user@ymul.com>
2025-10-08 13:32:34 +0000

    Updated Master

--- test.sh
+++ test.sh
@@ -1,7 +1,16 @@
-#!/usr/bin/bash
-cp  /var/log/*.log  ./verf
 
-for file in $(ls ./verf/*.log)
-do
-sed -i 's/error/Warning/g'  $file
+#!/usr/bin/bash
+
+cp /var/log/*.log /tmp
+
+for file in $(ls /tmp/*.log)
+
+
+do 
+if grep -q "host" $file
+then 
+          sed -i "s/host/Machine/1" $file
+fi
 done
+
+#==================================================
